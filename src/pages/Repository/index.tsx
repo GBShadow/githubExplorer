1fb943e77f649898629bf542625e2e88121cf6f5
GBShadow <gbs.shadow@gmail.com>
2020-06-08 01:51:12 -0300

    Create dashboad and persist

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return (
+    <>
+      <h1>Repository</h1>
+      <h1>Teste</h1>
+    </>
+  );
+};
+
+export default Repository;
